723c46b0536762c3414703c14e23fd70b9d8833f
Gunaseelan <Gunaseelan@DESKTOP-HIPIDNQ>
2023-07-25 20:01:19 +0530

    Second Commit

--- ActitimeProject/src/com/actitime/pom/EnterTimeTrackPage.java
+++ ActitimeProject/src/com/actitime/pom/EnterTimeTrackPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class EnterTimeTrackPage {
-	@FindBy(xpath = "//a[text()='Logout']")
+	@FindBy(xpath = "//a[@class='logout']")//a[text()='Logout' and @id='logoutLink']
 	private WebElement logOutBtn;
 	
 	@FindBy(xpath ="//div[text()='Tasks']")
